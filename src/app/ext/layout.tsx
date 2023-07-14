cf74621945f85a6c692eceba446d47ebc00d898d
Emad Ha <emad.haa@gmail.com>
2023-07-14 14:22:52 +0300

    .

--- src/app/ext/layout.tsx
+++ src/app/ext/layout.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import {metadata} from "@/app/dash/page";
-import {getStaticInfoIncludingLayouts} from "next/dist/build/entries";
-export async function getStaticProps() {
-    return { props: { title: 'HomePsssage' } }
-}
+
 export default function Layout({children}: { children: React.ReactNode }) {
 
     return <>
-        <h1>
-        </h1>
         {children}
     </>
 }
